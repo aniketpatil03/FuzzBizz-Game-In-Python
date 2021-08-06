7b50991884cb50087a781f208d9d3f4bd712a434
Aniket Patil <55218425+aniketpatil03@users.noreply.github.com>
2021-08-06 21:26:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 # Fuzz-Bizz game in which kids say fizz if no div by 3 & buzz if div by
 # 5 and if div by both then say BuzzFizz
 
-numbers = input")
+user_input = int(input("Enter the No:"))
 
 
-for numbers in range(1, 101):
+for numbers in range(user_input):
     if (numbers % 3) == 0 and (numbers % 5) != 0:
         print("Fizz", "\n")
 
